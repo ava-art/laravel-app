9a36929475ec9607a5168bf483e985dad885cb26
Vladimir <koshkadav@gmail.com>
2023-10-17 16:50:26 +0300

    Fisrst commit this is project

---
+++ database/migrations/2023_10_17_124201_create_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            //            $table->bigIncrements('id');
+            $table->string('title')->unique();
+            $table->string('slug')->unique();
+            $table->text('body');
+            $table->string('img');
+            //            $table->timestamp('published_at');
+            $table->timestamps();
+            // created_at
+            // updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
